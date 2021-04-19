a92a6c1df5b532f52ddaa7acf02fa9ed00c83cef
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-20 04:15:51 +0900

    Learn_Attributes
    learn the format of attributes as well as how to use the "Range" and "ExecuteInEditMode" attributes.

---
+++ Assets/Scripts/Attributes/ColorScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class ColorScript : MonoBehaviour
+{
+    void Start()
+    {
+        // GetComponent<MeshRenderer>().material.color = Color.red;
+        // renderer.sharedMaterial.color = Color.red;
+    }
+}
