48460cf1206743db5dcd5dca1a5dbc843f1e0754
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-19 23:40:41 +0900

    Learn_ Lists and Dictionaries
    How to create and use the List and Dictionary collections.

---
+++ Assets/Scripts/ListsAndDictionaries/BadGuy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;//This allows the IComparable Interface
+
+/*
+This is the class you will be storing the different collections.
+In order to use a collection's Sort() method, this class needs to implement the IComparable interface.
+*/
+
+public class BadGuy : IComparable<BadGuy>
+{
+    public string name;
+    public int power;
+
+    public BadGuy(string name, int power)
+    {
+        this.name = name;
+        this.power = power;
+    }
+
+    // This method is required by the IComparable interface.
+    public int CompareTo(BadGuy other)
+    {
+        if (other == null)
+        {
+            return 1;
+        }
+
+        // Return the difference in power.
+        return power - other.power;
+    }
+}
---
+++ Assets/Scripts/ListsAndDictionaries/SomeOtherClass_Lad.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SomeOtherClass_Lad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*
+        This is how you create a Dictionary.
+        Notice how this takes two generic terms.
+        In this case you are using a string and a BadGuy as your two values.
+        */
+        Dictionary<string, BadGuy> badguys = new Dictionary<string, BadGuy>();
+
+        BadGuy bg1 = new BadGuy("Harvey", 50);
+        BadGuy bg2 = new BadGuy("Magneto", 100);
+
+        // You can place variables into the Dictionary with the Add() method.
+        badguys.Add("gangster", bg1);
+        badguys.Add("mutant", bg2);
+
+        BadGuy magneto = badguys["mutant"];
+        BadGuy temp = null;
+
+        // This is a safer, but slow, method of accessing values in a dictionary.
+        // if(badguys.TryGetValue("bir", out temp))
+        if(badguys.TryGetValue("mutant", out temp))
+        {
+            Debug.Log("Match !!");
+            Debug.Log(temp.name + " " + temp.power);
+        }
+        else if(temp == null)
+        {
+            Debug.Log("Not Found Guy !!");
+        }
+    }
+}
