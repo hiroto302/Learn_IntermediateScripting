81777b87584774548ff53323a5b43c22c70b25d0
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-20 03:48:31 +0900

    Learn_Delegates
    How to create and use delegates to provide complex and dynamic functionality in your scripts.

---
+++ Assets/Scripts/Delegates/DelegateScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelegateScript : MonoBehaviour
+{
+    delegate void MyDelegate(int num);
+    MyDelegate myDelegate;
+
+    void Start()
+    {
+        myDelegate = PrintNum;
+        myDelegate(10);
+
+        myDelegate = DoubleNum;
+        myDelegate(10);
+    }
+
+    void PrintNum(int num)
+    {
+        print(num);
+    }
+    void DoubleNum(int num)
+    {
+        print (num * 2);
+    }
+}
---
+++ Assets/Scripts/Delegates/MulticastScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MulticastScript : MonoBehaviour
+{
+    delegate void MultiDelegate();
+    MultiDelegate multiDelegate;
+
+    void Start()
+    {
+        multiDelegate = multiDelegate + PowerUp + TurnRed;
+        multiDelegate();
+
+        if(multiDelegate != null)
+        {
+            multiDelegate -= PowerUp;
+            multiDelegate += TurnBlue;
+            multiDelegate();             // 格納した順に実行されるので青色に上書きされる
+        }
+
+    }
+
+    void PowerUp()
+    {
+        print ("Orb is powering up!");
+    }
+
+    void TurnRed()
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.red;
+    }
+    void TurnBlue()
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.blue;
+    }
+
+}
