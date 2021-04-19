99f74c9f8f68afd4f7c48c203c81447d69e6a6a6
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-19 14:05:13 +0900

    Learn_Properties

---
+++ Assets/Scenes/Scripts/Properties/Game_Property.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_Property : MonoBehaviour
+{
+    void Start()
+    {
+        Player_Property player1 = new Player_Property();
+        Player_Property player2 = new Player_Property();
+
+        // Properties can be used just like variables
+        player1.Experience = 5000;
+        int x = player1.Level;
+
+        player2.Level = 7;
+        int y = player2.Experience;
+
+        Debug.Log(x);
+        Debug.Log(y);
+    }
+}
---
+++ Assets/Scenes/Scripts/Properties/Player_Property.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// The Player Level is determined by experience.
+public class Player_Property : MonoBehaviour
+{
+    // Member variables can be referred to as fields.
+    private int _experience;
+    public int Experience
+    {
+        get
+        {
+            // some other code
+            return _experience;
+        }
+        set
+        {
+            // some other code
+            _experience = value;
+        }
+    }
+
+    // Level is a property that converts experience
+    // points into the level of a player automatically
+    public int Level
+    {
+        get
+        {
+            return _experience / 1000;
+        }
+        set
+        {
+            _experience = value * 1000;
+        }
+    }
+
+    //auto-implemented property
+    public int Health { get; set; }
+}
