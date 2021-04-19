234faab39d1f525e34c3dba1ebfb2d7ee414344c
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-20 03:18:47 +0900

    Learn_Quaternions
    How to utilize the quaternion system to manage the rotation of game objects.

---
+++ Assets/Scripts/Quaternions/GravityScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// orbit effect
+public class GravityScript : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset = new Vector3(0, 1.5f, 0);
+
+    void Update ()
+    {
+
+        Vector3 relativePos = (target.position + offset) - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(relativePos);
+
+        Quaternion current = transform.localRotation;
+
+        // lerp  : linear 一次関数
+        // Slerp : Curve  二次関数
+        transform.localRotation = Quaternion.Slerp(current, rotation, Time.deltaTime);
+
+        transform.Translate(0, 0, 3 * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Quaternions/LookAtScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtScript : MonoBehaviour
+{
+    public Transform target;
+
+    void Update ()
+    {
+        Vector3 relativePos = target.position - transform.position;
+        transform.rotation = Quaternion.LookRotation(relativePos);
+    }
+}
