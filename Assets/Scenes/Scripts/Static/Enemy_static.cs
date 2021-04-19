10d897744ff5dd51ee63a6e40afc5566224faa53
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-19 15:16:59 +0900

    Learn_static

---
+++ Assets/Scenes/Scripts/Static/Enemy_static.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_static : MonoBehaviour
+{
+    // Static variables are shared across all instances of a class.
+    public static int enemyCount;
+    public int noStaticVariable;
+
+    public Enemy_static()
+    {
+        enemyCount++;
+    }
+
+    /*
+    A static method can be invoked without an object of a class.
+    Note that static methods cannot access non-static member variables.
+    */
+    public static int AddHealth(int num1)
+    {
+        // return num1 + num2 + noStaticVariable;
+        return num1 + enemyCount;
+    }
+
+}
---
+++ Assets/Scenes/Scripts/Static/Game_static.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_static : MonoBehaviour
+{
+    void Start()
+    {
+        Enemy_static enemy1 = new Enemy_static();
+        Enemy_static enemy2 = new Enemy_static();
+
+        List<Enemy_static> enemys = new List<Enemy_static>();
+        enemys.Add(enemy1);
+        enemys.Add(enemy2);
+
+        // You can access a static variable by using the class name and the dot operator.
+        Debug.Log(Enemy_static.enemyCount);
+        int enemyHealth = Enemy_static.AddHealth(enemys.Count);
+        Debug.Log(enemyHealth);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
